d9dc3be3c2853bc0c4f13ffe4cde97272e607c61
ChrisByrn3 <chris.byrn3@gmail.com>
2023-04-11 16:39:37 +0100

    queries working

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 const express = require('express');
 const app = express();
-const { Joke } = require('./db');
+const { Joke, Op } = require('./db');
+//const {Op} = require('sequelize');
 
+app.set("json spaces", "\t");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/jokes', async (req, res, next) => {
-  try {
-    // TODO - filter the jokes by tags and content
-    const jokes = [];
+  if (req.query){
+    try{
+      const jokes = await Joke.findAll({
+        where: {
+          tags: {
+            [Op.substring]:req.query.tags ?? "",
+          },
+          joke: {
+            [Op.substring]:req.query.joke ?? "",
+          }
+        },
+      });
     res.send(jokes);
   } catch (error) {
     console.error(error);
     next(error)
   }
+  return;
+  }
+try {
+  const jokes = await Joke.findAll();
+  res.send(jokes);
+} catch (error) {
+  console.error(error);
+  next(error);
+}
 });
 
 // we export the app, not listening in here, so that we can run tests
